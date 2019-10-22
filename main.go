eda5e9c382b61978a5572ff026b3cbb4c2bef40f
wary <wary@qq.com>
2019-10-22 21:07:33 +0800

    oss

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+)
+
+const strEndpoint = "oss-cn-hangzhou.aliyuncs.com"
+const strAccessKeyID = "LTAIbkRnr6mkDoCI"
+const strAccessKeySecret = "DEFfqXmalyGtNK4GcVTe0djbq9B6cB"
+const strBucketName = "789test"
+
+func main() {
+	copyFile()
+}
+
+func copyFile() {
+	// 创建OSSClient实例。
+	client, err := oss.New(strEndpoint, strAccessKeyID, strAccessKeySecret)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+
+	bucketName := strBucketName
+	objectName := "余额宝客户端协议.json"
+	destObjectName := "Backup/20190831/余额宝客户端协议.json"
+
+	// 获取存储空间。
+	bucket, err := client.Bucket(bucketName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+
+	// 拷贝文件到同一个存储空间的另一个文件。
+	r, err := bucket.CopyObject(objectName, destObjectName)
+	fmt.Println(r, err)
+}
+
+func updateFile() {
+	// var strObjectName string
+	// var strLocalFile string
+	// flag.StringVar(&strObjectName, "o", "ObjectName", "ObjectName")
+	// flag.StringVar(&strLocalFile, "l", "LocalFileX", "LocalFileY")
+	// flag.Parse()
+
+	strObjectName := "余额宝客户端协议.json"
+	strLocalFile := "E:/ALL.sql"
+
+	// 创建OSSClient实例。
+	client, err := oss.New(strEndpoint, strAccessKeyID, strAccessKeySecret)
+	if err != nil {
+		fmt.Println("oss.New Error:", err)
+		os.Exit(-1)
+	}
+
+	// 获取存储空间。
+	bucket, err := client.Bucket(strBucketName)
+	if err != nil {
+		fmt.Println("Bucket Error:", err)
+		os.Exit(-1)
+	}
+
+	fmt.Println("上传的文件名是", strLocalFile)
+
+	// 上传本地文件。
+	err = bucket.PutObjectFromFile(strObjectName, strLocalFile)
+	if err != nil {
+		fmt.Println("PutObjectFromFile Error:", err)
+		os.Exit(-1)
+	}
+}
---
+++ oss/osslib.go
@@ -0,0 +1,71 @@
+package oss
+
+import (
+	"fmt"
+
+	oossss "github.com/aliyun/aliyun-oss-go-sdk/oss"
+)
+
+var client *oossss.Client
+
+// 定义进度条监听器。
+type OssProgressListener struct {
+}
+
+// 定义进度变更事件处理函数。
+func (listener *OssProgressListener) ProgressChanged(event *oossss.ProgressEvent) {
+	switch event.EventType {
+	// case oossss.TransferStartedEvent:
+	// 	fmt.Printf("传输开始, 已完成: %d, 总字节 %d.\n",
+	// 		event.ConsumedBytes, event.TotalBytes)
+	case oossss.TransferDataEvent:
+		fmt.Printf("\r%d%%.        ",
+			event.ConsumedBytes*100/event.TotalBytes)
+	case oossss.TransferCompletedEvent:
+		// fmt.Printf("\n传输完毕, 已完成: %d, 总字节 %d.\n",
+		// 	event.ConsumedBytes, event.TotalBytes)
+	case oossss.TransferFailedEvent:
+		// fmt.Printf("\n传输失败, 已完成: %d, 总字节 %d.\n",
+		// 	event.ConsumedBytes, event.TotalBytes)
+	default:
+	}
+}
+
+// InitSDK 初始化SDK
+func InitSDK(strAccessKeyID, strAccessKeySecret, strEndpoint string) {
+	c, err := oossss.New(strEndpoint, strAccessKeyID, strAccessKeySecret)
+
+	if err != nil {
+		fmt.Println("初始化SDK错误:", err)
+		return
+	}
+
+	client = c
+}
+
+// PutObject 快速上传
+func PutObject(strBucketName, strObjectName, strLocalFile string) bool {
+	if client == nil {
+		fmt.Println("没有对应的桶指针")
+		return false
+	}
+
+	bucket, err := client.Bucket(strBucketName)
+	if err != nil {
+		fmt.Println("打开桶[", strBucketName, "]错误: ", err)
+		return false
+	}
+
+	err = bucket.PutObjectFromFile(strObjectName, strLocalFile, oossss.Progress(&OssProgressListener{}))
+	if err != nil {
+		fmt.Println("上传文件错误: ", err)
+		return false
+	}
+
+	return true
+}
+
+// ReleaseSDK 释放内存
+func ReleaseSDK() {
+	client = nil
+}
